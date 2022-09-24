26bfce821d0509e27b97978dabacf6a00bf09c09
Chidi Duru <chidi.duru@student.griffith.ie>
2022-09-24 11:37:25 +0100

    Page not found completed

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import style from "./PageNotFound.module.css";
+
 const PageNotFound = () => {
-  return <>Page not found</>;
+  return (
+    <div className={`${style.bg}`}>
+      <h1>404</h1>
+      <div className={style.bar}>|</div>
+      <p className="mt-2">This page could not be found</p>
+      <Link to="/" className={style.link}>
+        <span className="d-none d-sm-inline">back to </span> HOME
+      </Link>
+    </div>
+  );
 };
 
 export default PageNotFound;
